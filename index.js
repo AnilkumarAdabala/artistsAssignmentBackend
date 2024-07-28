bc81e83796cba7e11282fa327ce8558597561db5
AnilkumarAdabala <anilkumara2254@gmail.com>
2024-07-28 20:58:03 +0530

    cors code chages

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 
 const { open } = require("sqlite");
 const sqlite3 = require("sqlite3");
 const app = express();
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+
 
 const dbPath = path.join(__dirname, "database.db");
 
@@ -32,23 +35,37 @@ app.post('/addTransaction', async (request, response) => {
     try {
         const transactionData = request.body;
         const { description, type, amount } = transactionData;
-        const sqlQuery = `
-        INSERT INTO transactions
-            (description, type, amount) 
-        VALUES (?, ?, ?)
-    `;
 
-        await db.run(sqlQuery, [description, type, amount]);
-        response.status(200).send("transaction added successfully!");
+        // Fetch the current balance from the database
+        const balanceQuery = `SELECT balance FROM transactions ORDER BY id DESC LIMIT 1`;
+        const lastTransaction = await db.get(balanceQuery);
+        let balance = lastTransaction ? lastTransaction.balance : 0;
+        console.log(balance)
+        // Adjust the balance based on the transaction type
+        if(type == "Credit"){
+            balance += amount;
+        } else {
+            balance -= amount;
+        }
+
+        const sqlQuery = `
+            INSERT INTO transactions
+                (description, type, amount, balance) 
+            VALUES (?, ?, ?, ?)
+        `;
+
+        await db.run(sqlQuery, [description, type, amount, balance]);
+        response.status(200).json({message:"Transaction added successfully!"});
     } catch (e) {
-        console.error(`Error adding note: ${e.message}`);
-        response.status(500).send("An error occurred while adding the note.");
+        console.error(`Error adding transaction: ${e.message}`);
+        response.status(500).send("An error occurred while adding the transaction.");
     }
 });
 
+
 app.get('/getTransactions', async (request, response) => {
     try {
-        const sqlQuery = `SELECT * FROM transactions ;`;
+        const sqlQuery = `SELECT * FROM transactions ORDER BY  date DESC;`;
 
         const transactions = await db.all(sqlQuery);
 
@@ -58,3 +75,15 @@ app.get('/getTransactions', async (request, response) => {
         response.status(500).send("An error occurred while retrieving the notes.");
     }
 });
+
+
+app.get("/", async (request,response) => {
+    try{
+        response.status(200).send("Welcome artistAssignment");
+    }catch (e){
+        console.log(`Error: ${e.message}`);
+        
+    }
+});
+
+module.exports = app;
